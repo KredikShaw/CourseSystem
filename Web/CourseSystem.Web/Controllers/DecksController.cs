cb04053e3fefb006ca0673551d3393363ce4da68
KredikShaw <alekskn999@gmail.com>
2020-03-15 11:42:15 +0200

    Deck page and controller created

---
+++ Web/CourseSystem.Web/Controllers/DecksController.cs
@@ -0,0 +1,17 @@
+﻿namespace CourseSystem.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DecksController : Controller
+    {
+        public IActionResult MyDecks()
+        {
+            return this.View("MyDecks");
+        }
+    }
+}
